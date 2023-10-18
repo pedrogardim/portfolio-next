c41eb05fb48e9649481ccf902e0d1d0f0e565295
Pedro Gardim <me@pedrogardim.com>
2023-10-19 00:15:25 +0200

    feat: home: add presentation text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import TagStyledText from "../components/ui/TagStyledText";
+
 export default function Home() {
   return (
     <main className="grid grid-cols-2 w-full h-full">
       <div className="grid grid-rows-home-column">
         <div></div>
-        <div></div>
+        <div className="flex flex-col justify-center gap-y-4">
+          <TagStyledText autoClosing className="text-4xl font-bold">
+            Hi!
+          </TagStyledText>
+          <span className="text-primary text-5xl font-bold">
+            I'm Pedro Gardim
+          </span>
+          <span className="text-white text-xl font-bold">
+            Full stack developer
+          </span>
+        </div>
         <div></div>
       </div>
       <div className="grid grid-rows-home-column">
--- components/ui/TagStyledText.tsx
+++ components/ui/TagStyledText.tsx
@@ -1,9 +1,11 @@
 interface StyledTextProps {
+  autoClosing?: boolean;
   children: string;
   className?: string;
 }
 
 export default function TagStyledText({
+  autoClosing = false,
   className,
   children,
 }: StyledTextProps) {
@@ -11,6 +13,7 @@ export default function TagStyledText({
     <span className={className}>
       <span className="text-secondary">{"<"}</span>
       <span className="text-primary">{children}</span>
+      {autoClosing && <span className="text-secondary">{"/"}</span>}
       <span className="text-secondary">{">"}</span>
     </span>
   );
