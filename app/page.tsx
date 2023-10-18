eda03284af4243a7812d87e868577466aa67c710
Pedro Gardim <me@pedrogardim.com>
2023-10-19 00:29:10 +0200

    feat: home: nav links

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,23 @@ export default function Home() {
       <div className="grid grid-rows-home-column">
         <div></div>
         <div className="flex flex-col justify-center gap-y-4">
-          <TagStyledText autoClosing className="text-4xl font-bold">
+          <TagStyledText autoClosing className="text-4xl">
             Hi!
           </TagStyledText>
           <span className="text-primary text-5xl font-bold">
             I'm Pedro Gardim
           </span>
-          <span className="text-white text-xl font-bold">
+          <span className="text-white text-2xl font-bold">
             Full stack developer
           </span>
         </div>
-        <div></div>
+        <div className="flex flex-col gap-y-4">
+          {["projects", "profile", "contact", "github", "cv"].map((option) => (
+            <TagStyledText button className="text-2xl" key={option}>
+              {option}
+            </TagStyledText>
+          ))}
+        </div>
       </div>
       <div className="grid grid-rows-home-column">
         <div></div>
--- components/ui/TagStyledText.tsx
+++ components/ui/TagStyledText.tsx
@@ -1,18 +1,26 @@
 interface StyledTextProps {
   autoClosing?: boolean;
+  button?: boolean;
   children: string;
   className?: string;
 }
 
 export default function TagStyledText({
   autoClosing = false,
+  button = false,
   className,
   children,
 }: StyledTextProps) {
   return (
-    <span className={className}>
+    <span className={className + " font-bold " + (button && "cursor-pointer")}>
       <span className="text-secondary">{"<"}</span>
-      <span className="text-primary">{children}</span>
+      <span
+        className={
+          "text-primary" + " " + (button && "hover:text-white underline")
+        }
+      >
+        {children}
+      </span>
       {autoClosing && <span className="text-secondary">{"/"}</span>}
       <span className="text-secondary">{">"}</span>
     </span>
