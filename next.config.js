e659d2ee0499891864a377c7662194222aa3b673
Pedro Gardim <me@pedrogardim.com>
2023-11-07 13:50:33 +0100

    temp: add mock image domains on next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.pexels.com", "upload.wikimedia.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
