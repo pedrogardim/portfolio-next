573d2bf2881425b6c0910a130d930bf62ea9916f
Pedro Gardim <me@pedrogardim.com>
2023-11-07 18:36:42 +0100

    fix: add `onClick`, `className` on Button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,15 +1,22 @@
 interface ButtonProps {
   variant?: "primary" | "secondary";
   children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
 }
 
-export default function Button({ variant = "primary", children }: ButtonProps) {
+export default function Button({
+  variant = "primary",
+  children,
+  onClick,
+  className,
+}: ButtonProps) {
   return (
     <button
-      className={
-        "px-4 py-2 text-lg hover:bg-white " +
-        (variant === "primary" ? "bg-primary" : "bg-secondary")
-      }
+      className={`px-4 py-2 text-lg hover:bg-white ${
+        variant === "primary" ? "bg-primary" : "bg-secondary"
+      } ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
