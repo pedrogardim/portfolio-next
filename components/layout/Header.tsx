434da38a46edb5eab90403c011f4425f26121d11
Pedro Gardim <me@pedrogardim.com>
2023-11-08 12:16:14 +0100

    feat: add link to header name

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import TagStyledText from "../ui/TagStyledText";
 
 export default function Header() {
   return (
     <header>
       <h1 className="fixed text-xl font-bold">
-        <TagStyledText className="text-2xl">pedrogardim</TagStyledText>
+        <Link href={"/"}>
+          <TagStyledText className="text-2xl">pedrogardim</TagStyledText>
+        </Link>
       </h1>
     </header>
   );
